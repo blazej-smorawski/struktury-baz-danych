0b6959b16e7168efdbfda0848edb356082c4e3f0
blazej-smorawski <blazej.smorawski@gmail.com>
2022-11-22 20:05:11 +0100

    Add blockdevice

--- proj-1/src/main.rs
+++ proj-1/src/main.rs
@@ -1,9 +1,54 @@
-use std::{fs::File, io::{SeekFrom, Seek, Read}};
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Seek, SeekFrom, Write},
+};
 
-fn main(){
-    let mut file = File::open("foo.txt").unwrap();
-    file.seek(SeekFrom::Start(2)).unwrap();
-    let aux: &mut [u8] = &mut [0; 2];
-    let _buf = file.read_exact(aux);
-    println!("Hello, world!");
+struct BlockDevice {
+    file: File,
+    block_size: usize,
+    block: Vec<u8>,
+}
+
+impl BlockDevice {
+    pub fn new(filename: String, blocksize: usize) -> Result<BlockDevice, std::io::Error> {
+        let file: File = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(filename)?;
+        let mut device = BlockDevice {
+            file: file,
+            block_size: blocksize,
+            block: Vec::<u8>::new(),
+        };
+        device.block.resize(device.block_size, 0u8);
+        Ok(device)
+    }
+
+    pub fn read(&mut self, offset: u64) -> Result<&Vec<u8>, std::io::Error> {
+        self.file.seek(SeekFrom::Start(offset))?;
+        self.file.read_exact(&mut self.block)?;
+        Ok(&self.block)
+    }
+
+    pub fn write(&mut self, offset: u64, buf: &Vec<u8>) -> Result<usize, std::io::Error> {
+        if buf.len() != self.block_size {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Trying to write a buffer with a size different than devices blocksize",
+            ));
+        }
+        self.file.seek(SeekFrom::Start(offset))?;
+        self.file.write(buf)
+    }
+}
+
+fn run() -> Result<(), std::io::Error> {
+    let mut device = BlockDevice::new("new5.txt".to_string(), 2)?;
+    let buf = device.write(20, &vec![0xDE, 0xDE, 0xAB])?;
+    println!("{:?}", buf);
+    Ok(())
+}
+
+fn main() {
+    run().expect("Error while running:");
 }
