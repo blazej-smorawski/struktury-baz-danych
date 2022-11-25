f3e39b855eb452c8da2debf50c93054eb6c82ea6
blazej-smorawski <blazej.smorawski@gmail.com>
2022-11-25 19:11:33 +0100

    Add `Record` type

--- proj-1/src/main.rs
+++ proj-1/src/main.rs
@@ -1,16 +1,22 @@
+pub mod record;
+
 use std::{
     fs::{File, OpenOptions},
     io::{Read, Seek, SeekFrom, Write},
 };
 
+use crate::record::{
+    Record,
+    IntRecord,
+};
 struct BlockDevice {
     file: File,
-    block_size: usize,
+    block_size: u64,
     block: Vec<u8>,
 }
 
 impl BlockDevice {
-    pub fn new(filename: String, blocksize: usize) -> Result<BlockDevice, std::io::Error> {
+    pub fn new(filename: String, blocksize: u64) -> Result<BlockDevice, std::io::Error> {
         let file: File = OpenOptions::new()
             .write(true)
             .create(true)
@@ -20,24 +26,24 @@ impl BlockDevice {
             block_size: blocksize,
             block: Vec::<u8>::new(),
         };
-        device.block.resize(device.block_size, 0u8);
+        device.block.resize(device.block_size as usize, 0u8);
         Ok(device)
     }
 
-    pub fn read(&mut self, offset: u64) -> Result<&Vec<u8>, std::io::Error> {
-        self.file.seek(SeekFrom::Start(offset))?;
+    pub fn read(&mut self, lba: u64) -> Result<&Vec<u8>, std::io::Error> {
+        self.file.seek(SeekFrom::Start(lba*self.block_size))?;
         self.file.read_exact(&mut self.block)?;
         Ok(&self.block)
     }
 
-    pub fn write(&mut self, offset: u64, buf: &Vec<u8>) -> Result<usize, std::io::Error> {
-        if buf.len() != self.block_size {
+    pub fn write(&mut self, lba: u64, buf: &Vec<u8>) -> Result<usize, std::io::Error> {
+        if buf.len() != self.block_size as usize {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::InvalidData,
                 "Trying to write a buffer with a size different than devices blocksize",
             ));
         }
-        self.file.seek(SeekFrom::Start(offset))?;
+        self.file.seek(SeekFrom::Start(lba*self.block_size))?;
         self.file.write(buf)
     }
 }
---
+++ proj-1/src/record.rs
@@ -0,0 +1,97 @@
+use byteorder::{ByteOrder, LittleEndian};
+use rand::Rng;
+
+pub trait Record {
+    fn new() -> Self;
+    fn from_bytes(&mut self, bytes: Vec<u8>) -> Result<(), std::io::Error>;
+    fn get_size(&self) -> u64;
+    fn from_string(&mut self, string: String) -> Result<(), std::io::Error>;
+    fn from_random(&mut self) -> Result<(), std::io::Error>;
+    fn print();
+}
+
+pub struct IntRecord {
+    numbers: [u32; 15],
+}
+
+impl Record for IntRecord {
+    fn new() -> Self {
+        IntRecord { numbers: [0; 15] }
+    }
+
+    fn from_bytes(&mut self, bytes: Vec<u8>) -> Result<(), std::io::Error> {
+        if self.get_size() != (bytes.len() as u64) {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Supplied data is not the same size as required to create the record",
+            ));
+        };
+
+        LittleEndian::read_u32_into(&bytes, &mut self.numbers);
+        Ok(())
+    }
+
+    fn get_size(&self) -> u64 {
+        return std::mem::size_of_val(&self.numbers) as u64;
+    }
+
+    fn from_string(&mut self, string: String) -> Result<(), std::io::Error> {
+        let mut numbers: Vec<u32> = string
+            .split_ascii_whitespace()
+            .map(|s| s.parse::<u32>().unwrap_or(0u32))
+            .collect::<Vec<u32>>();
+        numbers.resize(self.numbers.len(), 0);
+
+        self.numbers.copy_from_slice(&numbers);
+        Ok(())
+    }
+
+    fn from_random(&mut self) -> Result<(), std::io::Error> {
+        let mut rng = rand::thread_rng();
+        self.numbers = rng.gen();
+        Ok(())
+    }
+
+    fn print() {}
+}
+
+#[cfg(test)]
+mod tests {
+    use std::mem::{size_of};
+
+    use super::*;
+
+    #[test]
+    fn test_from_string() -> Result<(), std::io::Error> {
+        let mut record: IntRecord = IntRecord::new();
+        record.from_string("1 2 3 4 5 6 7".to_string())?;
+
+        assert_eq!(
+            record.numbers,
+            [1u32, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_bytes() -> Result<(), std::io::Error> {
+        let mut bytes: Vec<u8> = vec![0u8; 15*size_of::<u32>()];
+        bytes[0] = 1;
+        bytes[1] = 0;
+        bytes[2] = 0;
+        bytes[3] = 0;
+        bytes[4] = 2;
+        bytes[5] = 0;
+        bytes[6] = 0;
+        bytes[7] = 0;
+
+        let mut record: IntRecord = IntRecord::new();
+        record.from_bytes(bytes)?;
+
+        assert_eq!(
+            record.numbers,
+            [1u32, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        );
+        Ok(())
+    }
+}
